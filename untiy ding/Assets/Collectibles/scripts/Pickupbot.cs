2b5674baed90f6ed3af7f9f30cc42b023f726b36
ElisaZornig <elisazornig@gmail.com>
2018-03-21 15:46:50 +0100

    Collectibles done

---
+++ untiy ding/Assets/Collectibles/scripts/Pickupbot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupbot : MonoBehaviour {
+
+    public GameObject nietbot;
+    void OnTriggrEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            nietbot.SetActive(true);
+        }
+    }
+}
