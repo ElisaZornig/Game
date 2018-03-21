87c23a687ed8b03cdbf7e50624ce2e2309fed956
willemijnbout <36730026+willemijnbout@users.noreply.github.com>
2018-03-21 15:52:08 +0100

    Enemy dingen

---
+++ untiy ding/Assets/Devil/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float radius = 3f;
+
+    bool isFocus = false;
+    Transform player;
+
+    public virtual void Interact() {
+        //This method is meant to be overwritten
+        Debug.Log("Interacting with" + transform.name);
+    }
+
+
+
+
+    void OnDrawGizmosSelected() {
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+	
+}
---
+++ untiy ding/Assets/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public Interactable focus;
+    public LayerMask movementMask;
+
+    Camera cam;
+    // Use this for initialization
+    void Start() {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                //Move our player to what we hit
+
+                //Stop focusing any objects
+                RemoveFocus();
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    Interactable interactable = hit.collider.GetComponent<Interactable>();
+                    if (interactable != null)
+                    {
+                        SetFocus(interactable);
+                    }
+                }
+            }
+        }
+    }
+        void SetFocus(Interactable newFocus)
+    {
+        focus = newFocus;
+        }
+
+    void RemoveFocus() {
+        focus = null;
+    }
+    
+}
+
+    
